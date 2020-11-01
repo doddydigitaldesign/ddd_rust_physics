be5e176fde55191d2be06be12f8ad19641a9e6d0
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-11-01 11:04:12 +0100

    Remove target, gh actions, devcontainers, readme

--- target/debug/deps/ddd_rust_physics_example.d
+++
@@ -1,5 +0,0 @@
-E:\dev\Rust\ddd_rust_physics\target\debug\deps\ddd_rust_physics_example.exe: ddd_rust_physics_example\src\main.rs
-
-E:\dev\Rust\ddd_rust_physics\target\debug\deps\ddd_rust_physics_example.d: ddd_rust_physics_example\src\main.rs
-
-ddd_rust_physics_example\src\main.rs:
