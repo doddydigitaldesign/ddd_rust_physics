1b0c0e9e6bbf91f58dd3e09d66ec9d6dad85e4c9
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-11-01 10:59:34 +0100

    update .gitignore, add simple usage example

--- ddd_rust_physics_example/src/main.rs
+++ ddd_rust_physics_example/src/main.rs
@@ -1,4 +1,24 @@
-use ddd_rust_physics_2d;
+use ddd_rust_physics_2d::{
+    self,
+    dynamics::{acceleration::Acceleration, velocity::Velocity},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let velocity = Velocity::new(0.0, 0.0, 0.0);
+    let acceleration = Acceleration::new(0.0, 0.0, 0.0);
+    let circle = ddd_rust_physics_2d::shapes::circle::Circle::new(
+        0.0,
+        0.0,
+        1.0,
+        Some(0.0),
+        velocity,
+        acceleration,
+    );
+    let output = format!(
+        "Circle - Position: {:?}, Velocity: {:?}, Acceleration: {:?}",
+        circle.get_position(),
+        circle.get_velocity().get_velocity(),
+        circle.get_acceleration().get_linear_acceleration()
+    );
+    println!("{}", output);
 }
