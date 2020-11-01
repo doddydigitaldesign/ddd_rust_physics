87229c0778d9b10c191d57413787b5f876e5cb06
Daniel Sonne Lehnberg <doddy@doddy.se>
2020-11-01 10:44:43 +0100

    Create workspace

---
+++ ddd_rust_physics_example/src/main.rs
@@ -0,0 +1,4 @@
+use ddd_rust_physics_2d;
+fn main() {
+    println!("Hello, world!");
+}
---
+++ target/debug/deps/ddd_rust_physics_example.d
@@ -0,0 +1,5 @@
+E:\dev\Rust\ddd_rust_physics\target\debug\deps\ddd_rust_physics_example.exe: ddd_rust_physics_example\src\main.rs
+
+E:\dev\Rust\ddd_rust_physics\target\debug\deps\ddd_rust_physics_example.d: ddd_rust_physics_example\src\main.rs
+
+ddd_rust_physics_example\src\main.rs:
